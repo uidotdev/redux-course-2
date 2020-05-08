05ac7ca0e6585d2c08e4422a2ae825ac89a18a69
Tyler McGinnis <tylermcginnis33@gmail.com>
2020-05-07 19:09:15 -0600

    Code for 3-users

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux'
 import authedUser from './authedUser'
+import users from './users'
 
 export default combineReducers({
-  authedUser
+  authedUser,
+  users,
 })
